c56110b1ccd555ba74f1edc3faff57dabfb86d5f
clément moretti <98534268+daddykoala@users.noreply.github.com>
2023-01-03 20:42:53 +0100

    add:feature component offer en cours

--- my-app/src/components/accompagnement/Offer.jsx
+++ my-app/src/components/accompagnement/Offer.jsx
@@ -4,7 +4,7 @@ import PETITEENFANCE from '../../assets/formuleenseignant.png'
 import PROFESSIONEL from '../../assets/formuleprofessionel.png'
 
 import "./OfferStyles.scss";
-
+import { useWindowSize } from "../../service/screenSize";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import 'swiper/css/navigation';
@@ -18,6 +18,8 @@ function Offer() {
 
 
     const [visible, setVisible] = useState(false);
+    const { width } = useWindowSize()
+    console.log(width);
   
     const handleClick = () => {
       setVisible(!visible);
@@ -31,14 +33,16 @@ function Offer() {
          spaceBetween={100}
          slidesPerView={1}
         //  navigation
-         pagination={{ clickable: true }}
+        //  pagination={{ clickable: true }}
          scrollbar={{ draggable: true }}
          onSwiper={(swiper) => console.log(swiper)}
          onSlideChange={() => console.log('slide change')}>
         <SwiperSlide>
-        <article className="offer__card" onClick={handleClick}>
+        <article className="offer__card" >
           <h3>formule parent</h3>
-          <div className="offer__card__column">
+          <div >
+            { width < 600 && (
+            <div className="offer__card__column" onClick={handleClick}>
             <div className="offer__card__column__item"
             style={{ display: visible ? 'block' : 'none '}}>
             <ol>
@@ -60,7 +64,43 @@ function Offer() {
               <p>Possibilité d'encaissement en plusieurs fois.</p>
             </div>
             </div>
-          <img className="offer__card__column__picture"src={PARENT} alt="un père et son fils" />
+            <div className="offer__card__column__picture">
+
+          <img src={PARENT}  alt="un père et son fils" />
+            </div>
+              </div>
+              
+            )}
+            { width > 600 && (
+              <div className="offer__card__column">
+                <div className="offer__card__column__item"
+                >
+                <ol>
+                  <li> 1. L’écoute, le soutien.(temps ) </li>
+                  <li>
+                    2.Explorer les besoins physiologiques pour mieux se connaître.
+                  </li>
+                  <li>3. Mettre la joie au centre de la vie de famille.</li>
+                  <li>4. Accompagner les pleurs et les colères.</li>
+                  <li>5. Poser des limites à son enfant.</li>
+                  <li>6. Quand la colère nous emporte.</li>
+                  <li>7. Comment les enfants apprennent.</li>
+                  <li>8. Renaître à soi-même en accompagnant ses enfants.</li>
+                </ol>
+                <div className="offer__price">
+                  <p>Tarif early bird : en fonction de l'offre du moment</p>
+                  <p>Nécessité de s'engager pour les 8 ateliers </p>
+                  <p>Limité à 8 participants</p>
+                  <p>Possibilité d'encaissement en plusieurs fois.</p>
+                </div>
+                </div>
+                <div className="offer__card__column__picture">
+    
+              <img src={PARENT} alt="un père et son fils" />
+                </div>
+              </div>
+
+          )}
           </div>
           <div className="offer__card__column__price">
 
