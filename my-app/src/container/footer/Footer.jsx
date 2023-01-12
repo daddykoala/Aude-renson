16d296410541d167dbff7de36618f21b8ee5d971
clément moretti <98534268+daddykoala@users.noreply.github.com>
2023-01-12 14:00:25 +0100

    fix footer

--- my-app/src/container/footer/Footer.jsx
+++ my-app/src/container/footer/Footer.jsx
@@ -1,51 +1,63 @@
-import React from 'react'
+import React from "react";
 
-import { CiFacebook } from 'react-icons/ci';
-import { FiInstagram} from 'react-icons/fi';
-import { FiLinkedin } from 'react-icons/fi';
+import { CiFacebook } from "react-icons/ci";
+import { FiInstagram } from "react-icons/fi";
+import { FiLinkedin } from "react-icons/fi";
 
 import "./FooterStyles.scss";
 export default function Footer() {
   return (
     <footer className="footer-container ">
-    <div >
-      <p>Suivez-moi sur les réseaux sociaux:</p>
-      <ul className="social-media-column">
-        <li>
-        <a href="https://www.facebook.com/Donner-des-racines-pour-d%C3%A9velopper-leurs-ailes-107398071518922/" target="_blank" ><CiFacebook/></a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/donner.des.ailes18/" target="_blank" ><FiInstagram/></a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/aude-renson-83b3a68/" target="_blank" ><FiLinkedin/></a>
-        </li>
-      </ul>
-    </div>
-    <div >
-      <p>Liens utiles:</p>
-      <div className="links-column">
-        <ul>
-        <li>
-          <a href="#">Qui suis-je</a>
-        </li>
-        <li>
-          <a href="#">Politique de confidentialité</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
+      <div>
+        <h5>Suivez-moi sur les réseaux sociaux:</h5>
+        <div className="social-media-column">
+          <a
+            className="header__socials__facebook"
+            href="https://www.facebook.com/Donner-des-racines-pour-d%C3%A9velopper-leurs-ailes-107398071518922/"
+            target="_blank"
+          >
+            <CiFacebook />
+          </a>
 
-        </ul>
-        
-        <ul>
+          <a
+            className="header__socials__instagram"
+            href="https://www.instagram.com/donner.des.ailes18/"
+            target="_blank"
+          >
+            <FiInstagram />
+          </a>
 
-        <li>
-          <a href="#">FAQ</a>
-        </li>
-        </ul>
+          <a
+            className="header__socials__linkedin"
+            href="https://www.linkedin.com/in/aude-renson-83b3a68/"
+            target="_blank"
+          >
+            <FiLinkedin />
+          </a>
+        </div>
       </div>
-    </div>
-  </footer>
-  )
+      <div>
+        <h5>Liens utiles:</h5>
+        <div className="links-column">
+          <ul>
+            <li>
+              <a href="#">Qui suis-je</a>
+            </li>
+            <li>
+              <a href="#">Politique de confidentialité</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+
+          <ul>
+            <li>
+              <a href="#">FAQ</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 }
