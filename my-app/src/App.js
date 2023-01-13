4f1a48bfcfd3174b8af3b9bea5f7331bc3226e5a
clément moretti <98534268+daddykoala@users.noreply.github.com>
2023-01-13 10:18:32 +0100

    add:restructration site

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import Main from './container/main/main';
 import Header from './container/header/Header';
 import Footer from './container/footer/Footer';
 import About from './components/About/About';
+
 import { Routes,Route } from 'react-router-dom'
 import './App.css';
 
@@ -15,7 +16,10 @@ function App() {
       <Routes>
         <Route path='/' element={<Main/>}></Route>
         <Route path='/qui-suis-je' element={<About/>}></Route>
-
+        {/* <Route path='/parent' element={<About/>}></Route>
+        <Route path='/professionel' element={<About/>}></Route>
+        <Route path='/contact' element={<About/>}></Route>
+        <Route path='/Agenda' element={<About/>}></Route> */}
       </Routes>
       
       <Footer/>
